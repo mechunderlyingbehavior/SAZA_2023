354091a68ffca4cdce546889ac991c163ff4f73d
Ivy <57535126+Ivy421@users.noreply.github.com>
2023-09-07 14:43:24 +0800

    Add files via upload

--- SAZA_windows/User Script.py
+++ SAZA_windows/User Script.py
@@ -3,14 +3,24 @@
 
 # # User Script
 
+# In[1]:
+
+
+from MakeCSV import makecsv
+from ROIData import ROIData
+from Sparklines import Sparklines
+from SaveTriggers import SaveTriggers
+data_folder = input("input the folder that contains .tdms file:")
+makecsv(data_folder)
+ROIData(data_folder)
+Sparklines(data_folder)
+SaveTriggers(data_folder,0,1000,Bin_Lengths=['0-1000', '1000-2000', '2000-3000', '3000-5000','5000-8000','8000-11000', '11000-15000', '15000-20000'])
+
+
 # In[ ]:
 
 
-data_folder = input("input the folder that contains .tdms file:")
-from MakeCSV import makecsv
-from ROIData import ROIData
-makecsv(data_folder)
-ROIData(data_folder)
+
 
 
 # In[ ]:
