8db649cc1d24849772f390751c7458590330121d
Ivy <57535126+Ivy421@users.noreply.github.com>
2023-09-13 16:07:31 +0800

    Add files via upload

--- SAZA_MacBook/User Script.py
+++ SAZA_MacBook/User Script.py
@@ -3,9 +3,6 @@
 
 # # User Script
 
-# In[1]:
-
-
 from MakeCSV import makecsv
 from ROIData import ROIData
 from Sparklines import Sparklines
@@ -13,17 +10,8 @@ from SaveTriggers import SaveTriggers
 data_folder = input("input the folder that contains .tdms file:")
 makecsv(data_folder)
 ROIData(data_folder)
-Sparklines(data_folder)
-SaveTriggers(data_folder,0,1000,Bin_Lengths=['0-1000', '1000-2000', '2000-3000', '3000-5000','5000-8000','8000-11000', '11000-15000', '15000-20000'])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+#Sparklines(data_folder)
+#SaveTriggers(data_folder,0,1000,Bin_Lengths=['0-1000', '1000-2000', '2000-3000', '3000-5000','5000-8000','8000-11000', '11000-15000', '15000-20000'])
 
 
 
